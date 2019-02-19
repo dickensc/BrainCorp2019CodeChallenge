51b0a749bb6d7cba541b17f56d9541b3b84c893e
Charles Dickens <charlieadickens@gmail.com>
2019-02-18 17:54:45 -1000

    progress

--- PasswordParser.py
+++ PasswordParser.py
@@ -2,13 +2,36 @@ import json
 import sys
 import os
 import argparse
+import pandas as pd
 
 exit_status = os.EX_OK
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--password", type=str, help="passwd file path. Leave blank to default to ")
-parser.add_argument("-g", "--groups", type=str, help="group file path")
+parser.add_argument("-p", "--password", type=str, help="Password file path. Leave blank to default to '/etc/passwd'")
+parser.add_argument("-g", "--groups", type=str, help="Groups file path, Leave blank to default to '/etc/groups'")
 p = parser.parse_args()
+
+
+def parse_password_file() -> dict:
+    """
+    Method for parsing information in password file.
+    :return:
+    """
+    global exit_status
+
+    password_header = ['Username', 'Password', 'UID', 'GID', 'UID Info', 'Home directory', 'Command']
+
+    try:
+        password_df = pd.read_table(p.password, sep=':', comment='#', index_col=0, names=password_header)
+        assert(not password_df.isnull().any().any())
+    except FileNotFoundError as err:
+        print(err)
+        exit_status = os.EX_DATAERR
+        sys.exit(exit_status)
+    except (IndexError, AssertionError) as err:
+        print(err)
+    print(password_df)
+    return {}
 
 
 def main() -> None:
@@ -17,6 +40,8 @@ def main() -> None:
     :return: None
     """
     global exit_status
+
+    parse_password_file()
 
 
 if __name__ == "__main__":
